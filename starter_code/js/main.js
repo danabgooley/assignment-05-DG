291cb565853f537f6be2eda8bae1be51151e9038
danabgooley <dana.b.gooley@gmail.com>
2017-05-03 15:10:55 -0400

    Fixed syntax errors

--- starter_code/js/main.js
+++ starter_code/js/main.js
@@ -1,47 +1,34 @@
+$(document).ready(function(){
 
+	// recognize when user submits (on.click)
+	$("#submit-btn").click(changeCity);
+	// call input from user
 
+	// store user input
 
-// call input from user
-$("#city-type").val();
+	
 
-// store user input
+	// use event.preventDefault(); to prevent the page from reloading
 
-var city = $("#city-type").val();
-// recognize when user submits (on.click)
-$("#submit-btn").click(changeCity);
-// use event.preventDefault(); to prevent the page from reloading
+	// get first element from attribute ($.attr())
 
-// get first element from attribute ($.attr())
+	function changeCity() {
+		event.preventDefault();
+		var city = $("#city-type").val();
+		console.log("is this working?");
+		if (city == "New York"|| city == "NYC" || city == "New York City") {
+			 $("body").attr("class","nyc");
+		} else if (city == "San Francisco"|| city == "SF" || city == "Bay Area") {
+			 $("body").attr(".sf");
+		} else if (city == "Los Angeles"|| city == "LA" || city == "LAX") {
+			 $("body").attr(".la");
+		} else if (city == "Austin"|| city == "ATX") {
+			 $("body").attr(".austin");
+		} else if (city == "Sydney"|| city == "SYD") {
+			 $("body").attr(".sydney");
+		}
+	}
 
-
-
-function changeCity() {
-	event.preventDefault();
-	$("#city-type").submit();
-
-
-	if(city == "New York"|| city == "NYC" || city == "New York City").(function() {
-		 $("body").attr(".nyc");
-
-	});
-
-	if(city == "San Francisco"|| city == "SF" || city == "Bay Area").(function() {
-		 $("body").attr(".sf");
-	});
-
-	if(city == "Los Angeles"|| city == "LA" || city == "LAX").(function() {
-		 $("body").attr(".la");
-	});
-
-	if(city == "Austin"|| city == "ATX").(function() {
-		 $("body").attr(".austin");
-	});
-
-	if(city == "Sydney"|| city == "SYD").(function() {
-		 $("body").attr(".sydney");
-	});
-
-
-}
+});
 // end of ready function
 
