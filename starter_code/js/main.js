6c6b197e457520d90bd5470b48c785040bf7673f
danabgooley <dana.b.gooley@gmail.com>
2017-05-11 18:56:56 -0400

    Fixed some stuff on Assignment 5

--- starter_code/js/main.js
+++ starter_code/js/main.js
@@ -6,21 +6,22 @@ var cities = ["NYC", "SF", "LA", "ATX", "SYD"];
 // //use the forEach function to add them to the select menu
 // On selection, change background
 $("select").change(function() {
+// get the value of user selection
 var city = $("#city-type").val();
-
-if (city == "new york"|| city == "nyc" || city == "new york city") {
+// use conditionals
+if (city == "NYC") {
 			 $("body").attr("class","nyc");
-		} else if (city == "san francisco"|| city == "sf" || city == "bay area") {
+		} else if (city == "SF") {
 			 $("body").attr("class","sf");
-		} else if (city == "los angeles"|| city == "la" || city == "lax") {
+		} else if (city =="LA") {
 			 $("body").attr("class","la");
-		} else if (city == "austin"|| city == "atx") {
+		} else if (city == "ATX") {
 			 $("body").attr("class","austin");
-		} else if (city == "sydney"|| city == "syd") {
+		} else if (city == "SYD") {
 			 $("body").attr("class","sydney");
 		}
 });
-
+// ????
 	cities.forEach(function(element) {
 		$("select").append("<option>" + element + "</option>");
 	})
